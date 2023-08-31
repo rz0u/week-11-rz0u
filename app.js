9d250f40ec0773ff32084c2e36c684e2bea697b6
rz0u <rayhan.zou98@gmail.com>
2023-08-31 20:20:22 +0700

    finished user section

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import { databaseMiddleware } from "./middleware/database-middleware.js";
+import { userRouter } from "./routes/user-router.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
 // Database Connection
 app.use(databaseMiddleware);
+
+// Import Router
+app.use("/users", userRouter);
 
 // App Listen
 app.listen(port, () => {
